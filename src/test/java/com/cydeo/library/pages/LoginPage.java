15961984ede5c34b1652a5153867ab762896834d
Aili Wusiman <aliosmancanada@gmail.com>
2022-12-01 21:18:54 -0500

    I created the frame work

---
+++ src/test/java/com/cydeo/library/pages/LoginPage.java
@@ -0,0 +1,43 @@
+package com.cydeo.library.pages;
+
+import com.cydeo.library.util.ConfigurationReader;
+import com.cydeo.library.util.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class LoginPage {
+    public LoginPage() {
+        PageFactory.initElements(Driver.getDriver(), this);
+    }
+
+    @FindBy(id = "inputEmail")
+    public WebElement emailBox;
+
+    @FindBy(id = "inputPassword")
+    public WebElement passwordBox;
+
+    @FindBy(tagName = "button")
+    public WebElement loginButton;
+
+
+
+    public void login(String userType){
+
+        String username= ConfigurationReader.getProperty(userType+"_username");
+        String password=ConfigurationReader.getProperty("password");
+
+        emailBox.sendKeys(username);
+        passwordBox.sendKeys(password);
+        loginButton.click();
+
+    }
+
+    public void login(String email,String password){
+
+        emailBox.sendKeys(email);
+        passwordBox.sendKeys(password);
+        loginButton.click();
+
+    }
+}
